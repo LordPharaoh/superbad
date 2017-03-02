6079cb306d7fe7726c1ab862fc33acde518b6f01
PedroPachuca <pedritopachuca@me.com>
2017-03-02 14:55:45 -0800

    Fixed Saving

--- src/SuperbadGameMap.java
+++ src/SuperbadGameMap.java
@@ -1,4 +1,55 @@
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.net.URL;
 
-public class SuperbadGameMap {
+import javax.imageio.ImageIO;
+
+
+public class SuperbadGameMap extends GameMap{
+	
+	public SuperbadGameMap(Dimension dim) {
+		super();
+		createLevel(1);
+		openBackgroundImage();
+	}
+	
+	private void createPlatforms(int amtOfPlatforms) {
+		// TODO createPlatforms
+	}
+	private void createLevel(int level) {
+		createPlatforms(level*20);
+		createCharacter();
+	}
+	private void createCharacter() {
+		// TODO createCharacter
+	}
+
+	@Override
+	public void openBackgroundImage() {
+		try {
+			URL url = getClass().getResource("res/background.jpg");
+			Image image = ImageIO.read(url);
+			this.backgroundImage = image;
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+	@Override
+	public void tick() {
+		// TODO CollideBeforeMoves
+		for(MovingObject currObj: movers) {
+			currObj.move();
+		}
+	}
+
+	@Override
+	public void draw(Graphics g) {
+		for(Drawable drawThis: drawers) {
+			drawThis.draw(g);
+		}
+	}
 
 }
