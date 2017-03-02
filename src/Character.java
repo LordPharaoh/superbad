c070afb3da3b94258c2f81a8259b8c2ec39b95eb
Varun Iyer <vi.mail@protonmail.ch>
2017-03-02 14:51:14 -0800

    Fixed CharacteR

--- src/Character.java
+++ src/Character.java
@@ -1,16 +1,36 @@
+import java.awt.Graphics;
 import java.awt.Rectangle;
 
 public abstract class Character implements MovingObject, Drawable{
+	protected Vector location;
+	protected Vector dimension;
+	
+	public Character (Vector loc, Vector dim) {
+		location = loc;
+		dimension = dim;
+	}
+	
+	public abstract void move();
 
-public abstract Character (int x, int y, int width, int height) {
-  super(x,y,width,height);
-}
-  public abstract void move() {
-    //TODO
-  }
+	public Rectangle getBoundingRect() {
+		return new Rectangle(location.x, location.y, dimension.x, dimension.y);
+		//We need to have the location and Dimension variables for this.
+	}
+	@Override
+	public Vector getLocation() {
+		// TODO Auto-generated method stub
+		return location;
+	}
+	@Override
+	public boolean collision(MovingObject r) {
+		/** True if two MovingObjects are colliding **/
+		if(r == null || r.getBoundingRect() == null) return false;
+		return (getBoundingRect()).intersects(r.getBoundingRect());
+	}
+	
+	@Override
+	public abstract void draw(Graphics g);
+	@Override
+	public abstract void handleCollision(MovingObject m);
 
-  public Rectangle getBoundingRect() {
-    return new Rectangle(location.x, location.y, dimension.x, dimension.y);
-    //We need to have the location and Dimension variables for this.
-  }
 }
