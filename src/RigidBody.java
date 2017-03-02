e8e0bc3c1b0d77aa5db28b92b61f3ec0a8146fa0
Varun Iyer <vi.mail@protonmail.ch>
2017-03-02 14:33:25 -0800

    Added RigidBody

---
+++ src/RigidBody.java
@@ -0,0 +1,47 @@
+import java.awt.Graphics;
+import java.awt.Rectangle;
+
+
+public abstract class RigidBody implements Drawable, MovingObject{
+	/**RigidBody 
+		framework for things like platforms and boxes
+		that you jump and run into in game**/
+	protected Vector location, dimension;
+	public RigidBody(Vector loc, Vector dim) {
+		location = loc;
+		dimension = dim;
+	}
+	@Override
+	public void move() {
+		/** Empty because these things should be static **/
+	}
+
+	@Override
+	public Rectangle getBoundingRect() {
+		// TODO Auto-generated method stub
+		return new Rectangle(location.x, location.y, dimension.x, dimension.y);
+
+	}
+
+	@Override
+	public Vector getLocation() {
+		return location;
+	}
+
+	@Override
+	public void handleCollision(MovingObject m) {
+		/** empty because these shouldn't be affected by collisions **/
+	}
+
+	@Override
+	public boolean collision(MovingObject r) {
+		/** True if two MovingObjects are colliding **/
+		if(r == null || r.getBoundingRect() == null) return false;
+		return (getBoundingRect()).intersects(r.getBoundingRect());
+	}
+
+	@Override
+	/** To be implemented by child **/
+	public abstract void draw(Graphics g);
+	
+}
