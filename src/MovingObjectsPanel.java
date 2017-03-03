05ec28a27c9c672fce456a0e3101b4a06ae84e36
Nathan Hilal <whitetiger915@gmail.com>
2017-03-02 20:03:16 -0800

    Changed the keystroke assignments to wsad instead of arrow keys

--- src/MovingObjectsPanel.java
+++ src/MovingObjectsPanel.java
@@ -11,17 +11,17 @@ import javax.swing.Timer;
 
 
 public class MovingObjectsPanel extends JPanel {
-	
+
 	/**
-	 * 
+	 *
 	 */
 	private static final long serialVersionUID = 1L;
 	final Dimension defaultDim;// = new Dimension(800,600);
 	public static final int FRAME_RATE = 30;
 	GameMap gm;
 	private Timer t;// belongs to the class
-	
-	
+
+
 	public MovingObjectsPanel(Dimension dim) {
 		defaultDim = dim;
 		this.setPreferredSize(defaultDim);
@@ -35,35 +35,35 @@ public class MovingObjectsPanel extends JPanel {
 				gm.tick();// I tell the GameMap to tick... do what
 					// you do every time the clock goes off.
 				repaint();// naturally, we want to see the new view
-				
+
 				//This sketchy stuff requests a mouse location every frame so it repaints
 		        //MouseInfo.getPointerInfo().getLocation();
 			}
 		});
 		t.start();
 
-		
+
 	}
-	
+
 	private void setUpKeyMappings() {
 		// maps keys with actions...
 		//  The code below maps a KeyStroke to an action to be performed
 		// In this case I mapped the space bar key to the action named "shoot"
 		// Whenever someone hits the Space Bar the action shoot is sent out
 
-		//this.getInputMap().put(KeyStroke.getKeyStroke("SPACE"),"shoot");
-		this.getInputMap().put(KeyStroke.getKeyStroke("LEFT"),"left");
-		this.getInputMap().put(KeyStroke.getKeyStroke("RIGHT"),"right");
-		this.getInputMap().put(KeyStroke.getKeyStroke("UP"),"up");
-		this.getInputMap().put(KeyStroke.getKeyStroke("DOWN"),"down");
-		
-		//  This associates the command shoot with some action.  In this 
-		// case, the action triggers a shoot command invoked on my GameMap.  In general, whatever 
+		this.getInputMap().put(KeyStroke.getKeyStroke("SPACE"),"Jump");
+		this.getInputMap().put(KeyStroke.getKeyStroke("A"),"left");
+		this.getInputMap().put(KeyStroke.getKeyStroke("D"),"right");
+		this.getInputMap().put(KeyStroke.getKeyStroke("W"),"up");
+		this.getInputMap().put(KeyStroke.getKeyStroke("S"),"crouch");
+
+		//  This associates the command shoot with some action.  In this
+		// case, the action triggers a shoot command invoked on my GameMap.  In general, whatever
 		// goes in the actionPerformed method will be executed when a shoot command
 		// is sent...
-			
+
 		/*this.getActionMap().put("shoot",new AbstractAction(){
-		
+
 			private static final long serialVersionUID = 1L;
 
 			@Override
@@ -72,10 +72,10 @@ public class MovingObjectsPanel extends JPanel {
 				((SuperbadGameMap) gm).shoot();
 			}
 		});*/
-		
+
 		this.getActionMap().put("left",new AbstractAction(){
 			/**
-			 * 
+			 *
 			 */
 			private static final long serialVersionUID = 1L;
 
@@ -85,10 +85,10 @@ public class MovingObjectsPanel extends JPanel {
 				((SuperbadGameMap) gm).move("left");
 			}
 		});
-		
+
 		this.getActionMap().put("right",new AbstractAction(){
 			/**
-			 * 
+			 *
 			 */
 			private static final long serialVersionUID = 1L;
 
@@ -98,10 +98,10 @@ public class MovingObjectsPanel extends JPanel {
 				((SuperbadGameMap) gm).move("right");
 			}
 		});
-		
+
 		this.getActionMap().put("up",new AbstractAction(){
 			/**
-			 * 
+			 *
 			 */
 			private static final long serialVersionUID = 1L;
 
@@ -111,10 +111,10 @@ public class MovingObjectsPanel extends JPanel {
 				((SuperbadGameMap) gm).move("up");
 			}
 		});
-		
+
 		this.getActionMap().put("down",new AbstractAction(){
 			/**
-			 * 
+			 *
 			 */
 			private static final long serialVersionUID = 1L;
 
@@ -124,14 +124,14 @@ public class MovingObjectsPanel extends JPanel {
 				((SuperbadGameMap) gm).move("down");
 			}
 		});
-		
-		
-		this.requestFocusInWindow();		
+
+
+		this.requestFocusInWindow();
 	}
-	
-	
-	
-	
+
+
+
+
 	@Override
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
