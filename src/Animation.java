b779f27ed0fcad5c28b84b027abc975fbb8b3db5
Varun Iyer <vi.mail@protonmail.ch>
2017-03-07 14:29:25 -0800

    Finished Animation class
    It probably doesn't work. Who knows. I sure don't.

--- src/Animation.java
+++ src/Animation.java
@@ -5,44 +5,67 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.imageio.ImageIO;
 
 
 public class Animation {
+	public static final String[] IMG_EXTENSIONS = {"jpg", "png", "bmp" };
 	private List<Image> images;
 	private File resourceFile;
 	private int frameNumber;
+	
+	public boolean isImage(File f) {
+		for(String s : IMG_EXTENSIONS) {
+			if(f.getName().indexOf("."+s) != -1) return true;
+		}
+		return false;
+	}
 	
 	public void reset() {
 		frameNumber = 0;
 	}
 	public void draw(Graphics g, Vector location) {
 		// TODO Auto-generated method stub
-		//g.drawImage(images.get(frameNumber), location.x, location.y);
+		g.drawImage(images.get(frameNumber), location.x, location.y, null);
+		frameNumber++;
+		if(frameNumber > images.size()) frameNumber = 0;
 	}
 	
-	public Animation(String s) throws IOException {
+	public Animation(String s) throws InvalidAnimationDirectoryException {
 		this(s, new Vector(1, 1));
 	}
 	
-	public Animation(String s, Vector size) throws IOException {
-		URL url = this.getClass().getResource("res");
+	public Animation(String s, Vector size) throws InvalidAnimationDirectoryException {
+		URL url = this.getClass().getResource(s);
+
 		try {
 			resourceFile = new File(url.toURI());
 		} catch (URISyntaxException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		frameNumber = 0;
 		if(!resourceFile.isDirectory()) {
-			throw new IOException("Invalid Animation directory");
+			//TODO custom exception
+			throw new InvalidAnimationDirectoryException("Expected a directory; given a file");
 		}
-		for(int i = 0; i < resourceFile.listFiles().length; i++) {
-			Image image = ImageIO.read(url);
-			images.add(image.getScaledInstance(size.x, size.y, Image.SCALE_DEFAULT));
+		
+		File[] dir = resourceFile.listFiles();
+		List<File> imageFiles = new ArrayList<File>();
+		for (File f : dir) {
+			if(isImage(f)) imageFiles.add(f);
 		}
+		for (File imgf : imageFiles) {
+			try {
+				images.add(ImageIO.read(imgf).getScaledInstance(size.x, size.y, Image.SCALE_DEFAULT));
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
 	}
 
 }
