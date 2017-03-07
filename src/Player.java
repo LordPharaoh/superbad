815483dd66015e7d40f5e678d2c0b2c2deb53adb
PedroPachuca <pedritopachuca@me.com>
2017-03-07 14:40:33 -0800

    Fixed velocity and location

--- src/Player.java
+++ src/Player.java
@@ -10,23 +10,19 @@ public class Player extends Character{
 	}
 
 	public void moveLeft() {
-		updateLocation(new Vector(this.getLocation().x - SPEED, this.getLocation().y));
 		this.location = this.location.add(new Vector(-SPEED, this.location.y));
 		
 	}
 	public void moveRight() {
-		updateLocation(new Vector(this.getLocation().x + SPEED, this.getLocation().y));
 		this.location = this.location.add(new Vector(SPEED, this.location.y));
 	}
 	public void jump() {
-		updateLocation(new Vector(this.getLocation().x, this.getLocation().y - JUMP_HEIGHT));
-		if(this.velocity.y == 0) {
+		if(this.velocity.y == 0) { //TODO fix double jumps
 		this.velocity = new Vector(this.velocity.x, this.velocity.y - JUMP_HEIGHT);
 		}
 	}
 	public void crouch() {
 		//TODO crouch mech
-		//updateLocation(new Vector(this.getLocation().x, this.getLocation().y - CROUCH_HEIGHT));
 	}
 
 	@Override
