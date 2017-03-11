fc225662fdc990c72503186f3b08588b07ad9927
Nathan Hilal <whitetiger915@gmail.com>
2017-03-10 17:12:17 -0800

    I GOT THE JUMPING MECHANISM TOO WORK!!!!!!!!!!!!

--- src/Player.java
+++ src/Player.java
@@ -1,11 +1,13 @@
 import java.awt.Graphics;
 import java.awt.Rectangle;
+import java.util.ArrayList;
 
 public class Player extends Character{
 	public static final int WIDTH = 25;
 	public static final int HEIGHT = 100;
 	private boolean falling;
 	private Animation animation;
+	private ArrayList<String> moveDir = new ArrayList<>();
 	
 	public Player (Vector loc) {
 		super(loc, new Vector(WIDTH, HEIGHT));
@@ -20,15 +22,23 @@ public class Player extends Character{
 
 	public void moveLeft() {
 		this.location = this.location.add(new Vector(-SPEED, 0));
+		moveDir.add("Left");
 		
 	}
 	public void moveRight() {
 		
 		this.location = this.location.add(new Vector(SPEED, 0));
+		moveDir.add("Right");
 	}
 	public void jump() {
-		if(this.velocity.y == 0) {
-		this.velocity = new Vector(this.velocity.x + SPEED, this.velocity.y - JUMP_HEIGHT);
+		if(this.velocity.y == 0) { 
+			if(moveDir.get(moveDir.size() - 1) != null && (moveDir.get(moveDir.size() - 1).equalsIgnoreCase("right"))){
+				this.velocity = new Vector(this.velocity.x + SPEED, this.velocity.y - JUMP_HEIGHT);
+			}
+			else if(moveDir.get(moveDir.size() - 1) != null && (moveDir.get(moveDir.size() - 1).equalsIgnoreCase("left"))){
+				this.velocity = new Vector(this.velocity.x - SPEED, this.velocity.y - JUMP_HEIGHT);
+			}
+		
 //		if(colliding){
 //			this.velocity.x = 0;
 //		}
